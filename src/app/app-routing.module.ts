fb72892940e5f8f8e4d514f7605ff83cb0a7d8fe
MJ Schanne <mj.schanne@capgemini.com>
2019-08-22 14:14:24 -0400

    Organize basic app routing with lazy loading of gadgets module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'gadgets', loadChildren: () => import('./gadgets/gadgets.module').then(mod => mod.GadgetsModule) },
+  { path: 'home', component: HomeComponent },
+  { path: '*', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { GadgetsModule } from './gadgets/gadgets.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GadgetsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
